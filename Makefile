36f1a116654dcab6f47ae44a21e21c1ff103cb9e
Andrew Ruef <awruef@cs.umd.edu>
2017-07-30 11:00:14 -0400

    It compiles!

--- Makefile
+++ Makefile
@@ -1,11 +1,15 @@
 CONFIG ?= llvm-config
 
 CXXFLAGS = $(shell $(CONFIG) --cxxflags)
+LDFLAGS = $(shell $(CONFIG) --ldflags)
+LLVM_LIBS = $(shell $(CONFIG) --libs)
+SYSLIBS = $(shell $(CONFIG) --system-libs)
+LIBS = -lclangTooling -lclangFrontend -lclangDriver -lclangSerialization -lclangParse -lclangSema -lclangAnalysis -lclangAST -lclangEdit -lclangLex -lclangCodeGen -lclangBasic
 
 all: crabclang
 
 crabclang: crabclang.cpp 
-	$(CXX) -o crabclang crabclang.cpp $(CXXFLAGS)
+	$(CXX) -o crabclang crabclang.cpp $(CXXFLAGS) $(LDFLAGS) $(LIBS) $(LLVM_LIBS) $(SYSLIBS)
 
 clean:
 	rm crabclang 
--- crabclang.cpp
+++ crabclang.cpp
@@ -1,4 +1,41 @@
+#include <clang/AST/ASTConsumer.h>
+#include <clang/AST/RecursiveASTVisitor.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/FrontendAction.h>
+#include <clang/Rewrite/Core/Rewriter.h>
+#include <clang/Tooling/CommonOptionsParser.h>
+#include <clang/Tooling/Tooling.h>
+#include <llvm/ADT/STLExtras.h>
+#include <llvm/ADT/StringSwitch.h>
+#include <llvm/Option/OptTable.h>
+#include <llvm/Support/Path.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/Signals.h>
+#include <llvm/Support/TargetSelect.h>
 
-int main(int argc, char *argv[]) {
+using namespace clang::driver;
+using namespace clang::tooling;
+using namespace clang;
+using namespace llvm; 
+
+static cl::OptionCategory CrabCat("crabclang options");
+cl::opt<bool> Verbose("verbose",
+                      cl::desc("Print verbose information"),
+                      cl::init(false),
+                      cl::cat(CrabCat));
+
+int main(int argc, const char **argv) {
+  sys::PrintStackTraceOnErrorSignal(argv[0]);
+
+  // Initialize targets for clang module support.
+  InitializeAllTargets();
+  InitializeAllTargetMCs();
+  InitializeAllAsmPrinters();
+  InitializeAllAsmParsers();
+
+  CommonOptionsParser OptionsParser(argc, argv, CrabCat);
+  tooling::CommandLineArguments args = OptionsParser.getSourcePathList();
+  ClangTool Tool(OptionsParser.getCompilations(), args);
+
   return 0;
 }
